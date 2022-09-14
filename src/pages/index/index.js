2e4eb5783db8823de0f1568e1f8772029988265b
Иван Литовченко <ivan-dp@bk.ru>
2022-09-14 12:30:32 +0300

    trying to connect plugin into the project

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,10 @@
 import "./index.scss";
+
+(function ($) {
+  $(window).on("load", function () {
+    $("html").mCustomScrollbar({
+      axis: "y",
+      theme: "dark",
+    });
+  });
+})(jQuery);
