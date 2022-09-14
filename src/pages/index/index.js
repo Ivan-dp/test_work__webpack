c574211b483a7984adab0a14ac7706975c466f9a
Ivan Litovchenko <54504811+Ivan-dp@users.noreply.github.com>
2022-09-14 15:56:14 +0300

    Scroll plugin (#1)
    * test deploy
    
    * trying to connect plugin into the project
    
    * modified options of scrollbar plugin from JQuery

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,11 @@
 import "./index.scss";
+
+(function ($) {
+  $(window).on("load", function () {
+    $("body").mCustomScrollbar({
+      setHeight: "100vh",
+      scrollbarPosition: "inside",
+      autoHideScrollbar: true,
+    });
+  });
+})(jQuery);
